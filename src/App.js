d3135acfe0056783cc4ea5794d51bcda7bf943fc
piotr-skolorzynski <p.skolorzynski@gmail.com>
2022-06-12 22:16:22 +0200

    complete test

--- src/App.js
+++ src/App.js
@@ -8,6 +8,14 @@ function App() {
           <label htmlFor='email' className='form-label'>Email address</label>
           <input type="email" id="email" name="email" className="form-control" />
         </div>
+        <div className='mb-3'>
+          <label htmlFor='password' className='form-label'>Password</label>
+          <input type="password" id="password" name="password" className="form-control" />
+        </div>
+        <div className='mb-3'>
+          <label htmlFor='confirm-password' className='form-label'>Confirm password</label>
+          <input type="password" id="confirm-password" name="confirm-password" className="form-control" />
+        </div>
       </form>
     </div>
   );
--- src/App.test.js
+++ src/App.test.js
@@ -3,7 +3,11 @@ import App from './App';
 
 test("inputs should be initially empty", () => {
   render(<App />)
-  const emailInputElement = screen.getByRole("textbox");
+  const emailInputElement = screen.getByLabelText(/email/i);
+  const passwordInputElement = screen.getByLabelText("Password");
+  const confirmPasswordInputElement = screen.getByLabelText(/confirm password/i);
 
-  expect(emailInputElement.value).toBe("")
+  expect(emailInputElement.value).toBe("");
+  expect(passwordInputElement.value).toBe("");
+  expect(confirmPasswordInputElement.value).toBe("");
 })

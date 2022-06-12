ee19a5ddaca1333cf4319161231f21a60e0ff24b
piotr-skolorzynski <p.skolorzynski@gmail.com>
2022-06-12 20:54:05 +0200

    create email input and basic test to find it

--- src/App.js
+++ src/App.js
@@ -1,23 +1,14 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='container my-5'>
+      <form>
+        <div className='mb-3'>
+          <label htmlFor='email' className='form-label'>Email address</label>
+          <input type="email" id="email" name="email" className="form-control" />
+        </div>
+      </form>
     </div>
   );
 }
--- src/App.test.js
+++ src/App.test.js
@@ -1,17 +1,9 @@
 import { render, screen } from '@testing-library/react';
 import App from './App';
 
-test('renders learn react link', () => {
-  // 1) rendering the component we want to test
-  render(<App />);
+test("inputs should be initially empty", () => {
+  render(<App />)
+  const emailInputElement = screen.getByRole("textbox");
 
-  // 2) finding the elements
-  const linkElement = screen.getByText(/learn react/i);
-
-  // 3. we expect that this element is present in the document
-  expect(linkElement).toBeInTheDocument();
-});
-
-// test("description", () => {
-
-// })
+  expect(emailInputElement.value).toBe("")
+})

01384cad8da162e095dce0fafe9784baa2769d16
piotr-skolorzynski <p.skolorzynski@gmail.com>
2022-06-12 20:30:49 +0200

    the structure of a test block

--- src/App.test.js
+++ src/App.test.js
@@ -2,7 +2,16 @@ import { render, screen } from '@testing-library/react';
 import App from './App';
 
 test('renders learn react link', () => {
+  // 1) rendering the component we want to test
   render(<App />);
+
+  // 2) finding the elements
   const linkElement = screen.getByText(/learn react/i);
+
+  // 3. we expect that this element is present in the document
   expect(linkElement).toBeInTheDocument();
 });
+
+// test("description", () => {
+
+// })

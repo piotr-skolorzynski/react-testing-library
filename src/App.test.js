1e7e91599d2db80b4204b05460e646ccf524e345
piotr-skolorzynski <p.skolorzynski@gmail.com>
2022-08-09 09:01:58 +0200

    start with userEvents in tests

--- src/App.test.js
+++ src/App.test.js
@@ -1,4 +1,5 @@
 import { render, screen } from '@testing-library/react';
+import userEvent from '@testing-library/user-event';
 import App from './App';
 
 test("inputs should be initially empty", () => {
@@ -10,4 +11,11 @@ test("inputs should be initially empty", () => {
   expect(emailInputElement.value).toBe("");
   expect(passwordInputElement.value).toBe("");
   expect(confirmPasswordInputElement.value).toBe("");
+})
+
+test("should be able to type an email", () => {
+  render(<App />)
+  const emailInputElement = screen.getByRole("textbox", {name: /email/i});
+  userEvent.type(emailInputElement, "selena@gmail.com");
+  expect(emailInputElement.value).toBe("selena@gmail.com")
 })

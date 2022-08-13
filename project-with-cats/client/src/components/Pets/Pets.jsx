320e9033fae353cd4504861b23d4e3f0ef9e18a1
piotr-skolorzynski <p.skolorzynski@gmail.com>
2022-08-13 13:36:28 +0200

    modify Pets component create http request to fetch cats from server using axios modify Pets.test.js file create test to check if proper number of cards is diplayed

--- project-with-cats/client/src/components/Pets/Pets.jsx
+++ project-with-cats/client/src/components/Pets/Pets.jsx
@@ -1,9 +1,22 @@
+import { useState, useEffect } from "react";
 import "./Pets.css";
 import Filter from "../Filter/Filter";
 import Cards from "../Cards/Cards";
-import cats from "../../mocks/cats.json";
+import axios from "axios";
 
 const Pets = () => {
+
+    const [cats, setCats] = useState([]);
+
+    const fetchCats = async () => {
+        const response = await axios.get("http://localhost:4000/cats");
+        setCats(response.data)
+    };
+
+    useEffect(() => {
+        fetchCats();
+    },[]);
+
     return (
         <div className="conatainer">
             <div className="app-container">
--- project-with-cats/client/src/components/Pets/__test__/Pets.test.js
+++ project-with-cats/client/src/components/Pets/__test__/Pets.test.js
@@ -1,1 +1,11 @@
 import { render, screen } from "@testing-library/react";
+import Pets from "../../Pets/Pets";
+
+describe('Pets', () => {
+    test('should render correct amount of cards', async () => {
+        render(<Pets />);
+        const cards = await screen.findAllByRole("article");
+        expect(cards.length).toBe(5);
+    });
+    
+});

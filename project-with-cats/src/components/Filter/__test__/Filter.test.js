cec978e95316aa73fd4f94c842bbb4583f1d23ea
piotr-skolorzynski <p.skolorzynski@gmail.com>
2022-08-13 12:24:42 +0200

    create directory Filter with directpry __test__ and Filter.test.js file Create tests to check if it is possible to change selected value in filter

---
+++ project-with-cats/src/components/Filter/__test__/Filter.test.js
@@ -0,0 +1,26 @@
+import { render, screen } from "@testing-library/react";
+import Filter from "../Filter";
+import userEvent from "@testing-library/user-event";
+
+describe('Filter', () => {
+    test('should be able to change value of favourite select', () => {
+        render(<Filter />);
+        const select = screen.getByLabelText(/favourite/i);
+        expect(select.value).toBe(/any/i);
+        userEvent.selectOptions(select, "favourite");
+        expect(select.value).toBe(/favourite/i);
+        userEvent.selectOptions(select, "not favourite");
+        expect(select.value).toBe(/not favourite/i);
+    });
+
+    test('should be able to change value of gender select', () => {
+        render(<Filter />);
+        const select = screen.getByLabelText(/gender/i);
+        expect(select.value).toBe(/any/i);
+        userEvent.selectOptions(select, "female");
+        expect(select.value).toBe(/female/i);
+        userEvent.selectOptions(select, "male");
+        expect(select.value).toBe(/male/i);
+    });
+    
+});

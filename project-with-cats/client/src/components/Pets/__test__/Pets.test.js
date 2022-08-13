d1fa34166825934dbb78edaf7007f0c0493993a9
piotr-skolorzynski <p.skolorzynski@gmail.com>
2022-08-13 14:03:44 +0200

    install mocked server worker library to use as mocked server create mocked server in Pets.test.js

--- project-with-cats/client/src/components/Pets/__test__/Pets.test.js
+++ project-with-cats/client/src/components/Pets/__test__/Pets.test.js
@@ -1,5 +1,21 @@
 import { render, screen } from "@testing-library/react";
 import Pets from "../../Pets/Pets";
+import { response, rest } from "msw";
+import { setupServer } from "msw/node";
+import catsMock from "../../../mocks/cats.json";
+
+const server = setupServer(
+    rest.get("http://localhost:4000/cats", (req, res, ctx) => {
+        response(
+            ctx.status(200),
+            ctx.json(catsMock)
+        )
+    })
+);
+
+beforeAll(() => server.listen());
+afterEach(() => server.resetHandlers());
+afterAll(() => server.close());
 
 describe('Pets', () => {
     test('should render correct amount of cards', async () => {

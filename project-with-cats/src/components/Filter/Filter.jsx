f8d48c1f2dd504892fadd95bb998c0594a540429
piotr-skolorzynski <p.skolorzynski@gmail.com>
2022-08-13 12:38:10 +0200

    create Filter component modify test use string instead REGEX

--- project-with-cats/src/components/Filter/Filter.jsx
+++ project-with-cats/src/components/Filter/Filter.jsx
@@ -1,5 +1,23 @@
 const Filter = () => {
-    return <div></div>
+    return(
+        <div className="pet-filter-container">
+            <div className="filter-container">
+                <label htmlFor="favourite">Favourite</label>
+                <select name="favourite" id="favourite" className="form-select">
+                    <option value="any">Any</option>
+                    <option value="favourite">Favourite</option>
+                    <option value="not favourite">Not favourite</option>
+                </select>
+
+                <label htmlFor="gender">Gender</label>
+                <select name="gender" id="gender" className="form-select">
+                    <option value="any">Any</option>
+                    <option value="female">Female</option>
+                    <option value="male">Male</option>
+                </select>
+            </div>
+        </div>
+    ); 
 };
 
 export default Filter;
--- project-with-cats/src/components/Filter/__test__/Filter.test.js
+++ project-with-cats/src/components/Filter/__test__/Filter.test.js
@@ -6,21 +6,21 @@ describe('Filter', () => {
     test('should be able to change value of favourite select', () => {
         render(<Filter />);
         const select = screen.getByLabelText(/favourite/i);
-        expect(select.value).toBe(/any/i);
+        expect(select.value).toBe("any");
         userEvent.selectOptions(select, "favourite");
-        expect(select.value).toBe(/favourite/i);
+        expect(select.value).toBe("favourite");
         userEvent.selectOptions(select, "not favourite");
-        expect(select.value).toBe(/not favourite/i);
+        expect(select.value).toBe("not favourite");
     });
 
     test('should be able to change value of gender select', () => {
         render(<Filter />);
         const select = screen.getByLabelText(/gender/i);
-        expect(select.value).toBe(/any/i);
+        expect(select.value).toBe("any");
         userEvent.selectOptions(select, "female");
-        expect(select.value).toBe(/female/i);
+        expect(select.value).toBe("female");
         userEvent.selectOptions(select, "male");
-        expect(select.value).toBe(/male/i);
+        expect(select.value).toBe("male");
     });
     
 });

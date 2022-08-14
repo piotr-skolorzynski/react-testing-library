0eaa0fc7f36f4269c3fa3f08e9e3477b9070c5c7
piotr-skolorzynski <p.skolorzynski@gmail.com>
2022-08-14 10:57:17 +0200

    create first integration test to filter cats by gender

--- project-with-cats/client/src/components/Pets/__test__/Pets.test.js
+++ project-with-cats/client/src/components/Pets/__test__/Pets.test.js
@@ -3,6 +3,7 @@ import Pets from "../../Pets/Pets";
 import { response, rest } from "msw";
 import { setupServer } from "msw/node";
 import catsMock from "../../../mocks/cats.json";
+import userEvent from "@testing-library/user-event";
 
 const server = setupServer(
     rest.get("http://localhost:4000/cats", (req, res, ctx) => {
@@ -13,15 +14,28 @@ const server = setupServer(
     })
 );
 
+beforeEach(() => render(<Pets />));
 beforeAll(() => server.listen());
 afterEach(() => server.resetHandlers());
 afterAll(() => server.close());
 
 describe('Pets', () => {
     test('should render correct amount of cards', async () => {
-        render(<Pets />);
         const cards = await screen.findAllByRole("article");
         expect(cards.length).toBe(5);
     });
+
+    test('should filter for male cats', async () => {
+        const cards = await screen.findAllByRole("article");
+        userEvent.selectOptions(screen.getByLabelText(/gender/i), "male");
+        expect(screen.getAllByRole("article")).toStrictEqual([cards[1], cards[3]]);
+    });
+
+    test('should filter for female cats', async () => {
+        const cards = await screen.findAllByRole("article");
+        userEvent.selectOptions(screen.getByLabelText(/gender/i), "female");
+        expect(screen.getAllByRole("article")).toStrictEqual([cards[0], cards[2]], cards[4]);
+    });
+    
     
 });
